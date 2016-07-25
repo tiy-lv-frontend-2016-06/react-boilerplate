02c7c1a29988b0d6783cb4d288e5a8587210e742
Michael Sweeney <Michael@MacBook-Pro.local>
2016-07-25 10:52:30 -0700

    made chagnes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory, hashHistory } from 'react-router';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 // Layout
 import ExampleLayout from 'layout/example';
 
@@ -9,11 +16,13 @@ import ExampleLayout from 'layout/example';
 import ExampleComponent from 'ui/example';
 
 const Site = (
-  <Router history={hashHistory}>
-    <Route component={ExampleLayout}>
-      <Route path="/" component={ExampleComponent} />
-    </Route>
-  </Router>
+  <MuiThemeProvider>
+    <Router history={hashHistory}>
+      <Route component={ExampleLayout}>
+        <Route path="/" component={ExampleComponent} />
+      </Route>
+    </Router>
+  </MuiThemeProvider>
 );
 
 render(Site, document.getElementById('app'));
--- src/ui/example.js
+++ src/ui/example.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import {Link} from 'react-router';
 
 export default React.createClass({
   render: function () {
     return (
       <div>
-        <p>I'm in UI</p>
+        <Drawer>
+          <Link to="/"><MenuItem>Home</MenuItem></Link>
+          <Link to="/example"><MenuItem>Example</MenuItem></Link>
+        </Drawer>
       </div>
     )
   }
